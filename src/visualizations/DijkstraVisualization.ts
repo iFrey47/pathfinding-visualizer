a2c65f780e1c5bc03f0cdb71e9e0f8a72417562f
muhammadammar2 <uExistentialist@proton.me>
2024-07-12 00:15:16 +0500

    Dijkstra's Visualization

--- src/visualizations/DijkstraVisualization.ts
+++ src/visualizations/DijkstraVisualization.ts
@@ -1,39 +1,7 @@
-// import { CellType } from "../components/Grid";
-// import { dijkstra } from "../algorithms/Dijkstra";
-
-// // visualize Dijkstra Algorithm
-// export const visualizeDijkstra = async (
-//   grid: CellType[][],
-//   start: { row: number; col: number },
-//   end: { row: number; col: number },
-//   setGrid: React.Dispatch<React.SetStateAction<CellType[][]>>
-// ) => {
-//   const path = dijkstra(grid, start, end);
-
-//   for (const node of path) {
-//     grid[node.row][node.col].isVisited = true;
-//     setGrid([...grid]);
-//     await new Promise((resolve) => setTimeout(resolve, 30)); // delay for visualization
-//   }
-
-//   // highlight the path to the end node
-//   if (
-//     path.length > 0 &&
-//     path[path.length - 1].row === end.row &&
-//     path[path.length - 1].col === end.col
-//   ) {
-//     for (const node of path) {
-//       grid[node.row][node.col].isPath = true;
-//       setGrid([...grid]);
-//       await new Promise((resolve) => setTimeout(resolve, 0)); // delay for path highlighting
-//     }
-//   }
-// };
-
 import { CellType } from "../components/Grid";
 import { dijkstra } from "../algorithms/Dijkstra";
 
-// visualize Dijkstra Algorithm with enhanced visuals
+// visualize Dijkstra Algorithm
 export const visualizeDijkstra = async (
   grid: CellType[][],
   start: { row: number; col: number },
